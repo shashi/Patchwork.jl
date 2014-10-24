e41c4117c401c4428bcb5cc78754ed6d392ac66e
Shashi Gowda <shashigowda91@gmail.com>
2014-10-24 21:47:23 +0530

    Almost rewrote diffing algorithm. Much better.
    * diff(a::Node, b::Node) - pure function returns a list of patches
    * diff!(a,b,index,patches) - appends patches to patches arg
    * exact correspondence of node indices to those in virtual-dom"

--- src/diff.jl
+++ src/diff.jl
@@ -1,103 +1,121 @@
 export diff
 
 abstract Patch
-type Replace <: Patch
-    a
+
+type Insert <: Patch
     b
 end
 
-type VectorDiff <: Patch
-    moves
-    inserts
-    deletes
+type Overwrite <: Patch
+    b
 end
-VectorDiff() = VectorDiff(Dict(), Dict(), Dict())
+
+type Delete <: Patch end
+
+type Reorder <: Patch
+    moves
+end
+
+type DictDiff <: Patch
+    updated
+    deleted
+end
 
 function key_idxs(ns; keyfn=key)
     i = 1
     idxs = Dict()
     for x in ns
+        k = keyfn(x)
         if keyfn(x) != nothing
-            idxs[keyfn(x)] = i
+            idxs[k] = i
+        else
+            idxs[i] = i
         end
         i += 1
     end
     idxs
 end
 
-function diff(a::AbstractVector, b::AbstractVector; keyfn=key)
-    if a === b return nothing end
+function diff!(a::AbstractVector, b::AbstractVector, index, patches; parentpatch=Patch[], keyfn=key)
+    if a === b return parentpatch end
 
-    if length(a) == 1 && length(b) == 1
-        return diff(a[1], b[1])
+    len_a = length(a)
+    len_b = length(b)
+
+    if len_a == 1 && len_b == 1
+        diff!(a[1], b[1], index+1, patches)
+        return parentpatch
     end
 
-    n = max(length(a), length(b))
     a_key_idxs = key_idxs(a)
     b_key_idxs = key_idxs(b)
 
-    patch = VectorDiff()
-    for i = 1:length(a)
-        moved_to = get(b_key_idxs, keyfn(a[i]), 0)
-        if moved_to == 0
-            patch.deletes[i] = Delete(a[i])
-        else
-            d = diff(a[i], b[moved_to])
-            if i == moved_to && d == nothing continue end
-            patch.moves[i] = (moved_to, d)
+    ord = Dict()
+    n = max(len_a, len_b)
+
+    index += 1
+    for i = 1:n
+        if i <= len_a
+            moved_to = get(b_key_idxs, keyfn(a[i]), 0)
+            if moved_to == 0
+                patches[index] = Delete()
+            else
+                diff!(a[i], b[moved_to], index, patches)
+                if i != moved_to
+                    ord[i] = moved_to
+                end
+            end
+        end
+        if i <= len_b
+            moved_from = get(a_key_idxs, keyfn(b[i]), 0)
+            if moved_from == 0
+                # We don't need to save where this goes
+                parentpatch = push!(parentpatch, Insert(b[i]))
+            end
+        end
+        if i <= len_a
+            index += count(a[i])
         end
     end
-
-    for i = 1:length(b)
-        moved_from = get(a_key_idxs, keyfn(b[i]), 0)
-        if moved_from == 0
-            patch.inserts[i] = Insert(b[i])
-        end
+    if !isempty(ord)
+        push!(parentpatch, Reorder(ord))
     end
-    patch
 end
 
-type ElemDiff <: Patch
-    a
-    attributes
-    children
-end
+diff!(a::Text, b::Text, index, patches) =
+    a === b || a.text == b.text ?
+        nothing :
+        patches[index] = Overwrite(b)
 
-type DictDiff <: Patch
-    added
-    updated
-    deleted
-end
+diff!(a::Elem, b::Text, index, patches) =
+    patches[index] = Overwrite(b)
 
-type Insert <: Patch
-    b
-end
+diff!(a::Text, b::Elem, index, patches) =
+    patches[index] = Overwrite(b)
 
-type Delete <: Patch
-    a
-end
+# If element's tag or ns is changed
+diff!(a::Elem, b::Elem, index, patches) =
+    patches[index] = Overwrite(b)
 
-function diff(a::Text, b::Text)
-    if a === b || a.text == b.text
-        return nothing
+function diff!{ns, tag}(a::Elem{ns, tag}, b::Elem{ns, tag}, index, patches)
+    if a === b return patches end
+
+    patch = get(patches, index, Patch[])
+
+    attrpatch = diff(a.attributes, b.attributes)
+    if !is(attrpatch, nothing)
+        patch = push!(patch, attrpatch)
     end
-    return Replace(a, b)
-end
 
-function diff{ns, tag}(a::Elem{ns, tag}, b::Elem{ns, tag})
-    if a === b return nothing end
-
-    attributes = diff(a.attributes, b.attributes)
-    children   = diff(a.children, b.children)
-    if !is(attributes, nothing) || !is(children, nothing)
-        return ElemDiff(a, attributes, children)
+    diff!(a.children, b.children, index, patches, parentpatch=patch)
+    if !isempty(patch)
+        patches[index] = patch
     end
 end
 
 function diff(a::Associative, b::Associative)
     if a === b return nothing end
 
-    added = Any[]
     updated = Any[]
     deleted = Any[]
     for (k, v) in a
@@ -115,75 +133,18 @@ function diff(a::Associative, b::Associative)
     end
     for (k, v) in b
         if k in a continue end
-        push!(added, (k, v))
+        push!(updated, (k, v))
     end
-    if isempty(added) && isempty(updated) && isempty(deleted)
+    if isempty(updated) && isempty(updated) && isempty(deleted)
         return nothing
     else
-        DictDiff(added, updated, deleted)
+        DictDiff(updated, deleted)
     end
 end
 
-# Pretty printing diffs
-function writestring(io::IO, d::VectorDiff)
-    for (x, y) in d.moves
-        write(io, string(x), " |> ", y[1], " # ", y[2], "\n")
-    end
-    for (i, x) in [d.inserts]
-        write(io, string(i), " +> ")
-        writestring(io, x)
-        write(io, "\n")
-    end
-    for (i, x) in [d.deletes]
-        write(io, string(i), " -> ")
-        writestring(io, x)
-        write(io, "\n")
-    end
-end
-
-function writestring(io::IO, p::Replace)
-    write(io, "- ")
-    writehtml(io, p.a)
-    write("\n+ ")
-    writehtml(io, p.b)
-end
-
-function writestring(io::IO, p::ElemDiff)
-    write(io, "~ ", tohtml(p.a))
-    writestring(io, p.attributes)
-    writestring(io, p.children)
-end
-
-function writestring(io::IO, p::DictDiff)
-    write(io, "\n- ")
-    map(a -> writehtml(io, a), p.deleted)
-    write(io, "\n+ ")
-    map(a -> writehtml(io, a), p.added)
-end
-
-function writestring(io::IO, p::Insert)
-    write(io, "I ")
-    writehtml(io, p.b)
-end
-
-function writestring(io::IO, p::Delete)
-    write(io, "D ")
-    writehtml(io, p.a)
-end
-
-function writestring(io::IO, ps::AbstractArray{Patch})
-    for p in ps
-        writestring(io, p)
-    end
-end
-
-function writestring(io::IO, x)
-    write(io, string(x))
-end
-
-function tostring(p::Patch)
-    io = IOBuffer()
-    writestring(io, p)
-    takebuf_string(io)
+function diff(a::Node, b::Node)
+    patches = Dict()
+    diff!(a, b, 0, patches)
+    return patches
 end
 
