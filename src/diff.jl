3483c11b21a59235e5884ec8b25c557e7e7bab55
Shashi Gowda <shashigowda91@gmail.com>
2014-10-25 15:59:19 +0530

    Fix index calculation bugs in Vector diff. Get type-stability with Overwrite.

--- src/diff.jl
+++ src/diff.jl
@@ -20,22 +20,26 @@ type DictDiff <: Patch
     updated
 end
 
-function key_idxs(ns; keyfn=key)
-    i = 1
+function key_idxs(ns)
+    i = j = 1
     idxs = Dict()
+    keys = Any[]
     for x in ns
-        k = keyfn(x)
-        if keyfn(x) != nothing
-            idxs[k] = i
+        k = key(x)
+        if k == nothing
+            idxs[j] = i
+            push!(keys, j)
+            j += 1
         else
-            idxs[i] = i
+            idxs[k] = i
+            push!(keys, k)
         end
         i += 1
     end
-    idxs
+    idxs, keys
 end
 
-function diff!(a::AbstractVector, b::AbstractVector, index, patches; parentpatch=Patch[], keyfn=key)
+function diff!(a::AbstractVector, b::AbstractVector, index, patches; parentpatch=Patch[])
     if a === b return parentpatch end
 
     len_a = length(a)
@@ -46,34 +50,32 @@ function diff!(a::AbstractVector, b::AbstractVector, index, patches; parentpatch
         return parentpatch
     end
 
-    a_key_idxs = key_idxs(a)
-    b_key_idxs = key_idxs(b)
+    a_key_idxs, a_keys = key_idxs(a)
+    b_key_idxs, b_keys = key_idxs(b)
 
     ord = Dict()
     n = max(len_a, len_b)
 
-    index += 1
     for i = 1:n
+        index += 1
         if i <= len_a
-            moved_to = get(b_key_idxs, keyfn(a[i]), 0)
+            moved_to = get(b_key_idxs, a_keys[i], 0)
             if moved_to == 0
                 patches[index] = Delete()
             else
                 diff!(a[i], b[moved_to], index, patches)
+                index += count(a[i])
                 if i != moved_to
                     ord[i] = moved_to
                 end
             end
         end
         if i <= len_b
-            moved_from = get(a_key_idxs, keyfn(b[i]), 0)
+            moved_from = get(a_key_idxs, b_keys[i], 0)
             if moved_from == 0
                 # We don't need to save where this goes
                 parentpatch = push!(parentpatch, Insert(b[i]))
             end
-        end
-        if i <= len_a
-            index += count(a[i])
         end
     end
     if !isempty(ord)
@@ -84,17 +86,17 @@ end
 diff!(a::Text, b::Text, index, patches) =
     a === b || a.text == b.text ?
         nothing :
-        patches[index] = Overwrite(b)
+        patches[index] = Patch[Overwrite(b)]
 
 diff!(a::Elem, b::Text, index, patches) =
-    patches[index] = Overwrite(b)
+    patches[index] = Patch[Overwrite(b)]
 
 diff!(a::Text, b::Elem, index, patches) =
-    patches[index] = Overwrite(b)
+    patches[index] = Patch[Overwrite(b)]
 
 # If element's tag or ns is changed
 diff!(a::Elem, b::Elem, index, patches) =
-    patches[index] = Overwrite(b)
+    patches[index] = Patch[Overwrite(b)]
 
 function diff!{ns, tag}(a::Elem{ns, tag}, b::Elem{ns, tag}, index, patches)
     if a === b return patches end
